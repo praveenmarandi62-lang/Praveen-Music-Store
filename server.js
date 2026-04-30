22a694ed1ed43089b6c54a816f7657abb1d86048
praveenmarandi62-lang <praveenmarandi62@gmail.com>
2026-05-01 00:02:06 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ dotenv.config();
 const app = express();
 
 /* Middleware */
-app.use(cors());
+app.use(cors({
+  origin: "https://praveenmarandi62-lang.github.io"
+}));
 app.use(express.json());
 
 /* Cloudinary Connect */
